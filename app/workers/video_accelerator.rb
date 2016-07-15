9cfec1532fa1d60f99ad2cc43b606c10e294b757
vitoraraujo <vitoraraujow@gmail.com>
2016-07-15 16:18:13 -0300

    Navegador e a unica parte troll

--- app/workers/video_accelerator.rb
+++ app/workers/video_accelerator.rb
@@ -1,3 +1,10 @@
 class VideoAccelerator
-@queue
+	@queue = :posts_queue
+
+	def self.perform(post_id)
+		post = Post.find(post_id)
+		init = `mv #{post.video.path} #{post.video.path}input.mp4`
+		command = `ffmpeg -i #{post.video.path}input.mp4 -filter:v "setpts=0.5*PTS" #{post.video.path}`
+
+	end
 end
